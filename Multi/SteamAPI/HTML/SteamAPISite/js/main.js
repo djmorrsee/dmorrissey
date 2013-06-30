6ed68086240c17b9eff73e3c48ad1d983d873daa
djmorrsee <djmorrsee@gmail.com>
2013-06-30 15:20:33 -0700

    Factoring/ListFactoring/Multithreading

---
+++ Multi/SteamAPI/HTML/SteamAPISite/js/main.js
@@ -0,0 +1,4 @@
+
+
+var x = $.get("https://api.steampowered.com/IDOTA2Match_570/GetMatchHistory/V001/?key=<06F26E71DA72FD03C5A1304C565EAA9E>")
+console.log(x)
---
+++ Multi/SteamAPI/documentation.md
@@ -0,0 +1,4 @@
+
+Returns last 25 public matches
+
+https://api.steampowered.com/IDOTA2Match_570/GetMatchHistory/V001/?key=<06F26E71DA72FD03C5A1304C565EAA9E>
